cb0d8e5b8c3d6863537c6ade87da29e0ed92b0bc
ndorsett333 <nicolasdorsett@gmail.com>
2019-05-03 14:20:38 -0600

    9.99

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const ImageList = props => {
-  console.log(props.images);
+  const images = props.images.map(image => {
+    return (
+      <img alt={image.description} key={image.id} src={image.urls.regular} />
+    );
+  });
 
-  return <div>ImageList</div>;
+  return <div>{images}</div>;
 };
 
 export default ImageList;
